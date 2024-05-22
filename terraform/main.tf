716a5f70893277a81cb3646506111634a1472a92
nazimgueye <nazimgueye17@gmail.com>
2024-05-22 14:05:18 +0000

    Ansible + terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,20 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
     }
   }
-
-  required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region  = "us-east-1"
+provider "kubernetes" {
+  config_path = "~/.kube/config"
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "fil-rouge"
-  public_key = file("public-key.pub")
+# Terraform provisionne uniquement le cluster Kubernetes ici
+# Les déploiements et services Kubernetes seront gérés par Ansible
+
+output "kube_config" {
+  value = file("~/.kube/config")
+  sensitive = true
 }
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-0bb84b8ffd87024d8"
-  instance_type = "t2.micro"
-  key_name        = aws_key_pair.deployer.key_name
-
-  tags = {
-    Name = "fil-rouge"
-  }
-}
-
